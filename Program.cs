0673988446146722a6fb455ef98f40e51d00459f
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-13 23:07:24 -0300

    Adicionada a variável média e solicitada a média final do aluno

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace Conceito
     {
         static void Main(string[] args)
         {
+            double media;
             Console.WriteLine("Conceito média.");
+            Console.WriteLine("digite a média final do aluno.");
+            media = double.Parse(Console.ReadLine());
         }
     }
 }
