f3b332898443002e6e32b951d90137a3e4e4b991
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-13 23:23:15 -0300

    adicionada a função para retornar conceito, e projeto finalizado.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,36 @@ namespace Conceito
 {
     class Program
     {
+
+        public static string retornaConceito(double media)
+        {
+            if (media <= 4.9)
+            {
+                return ("d.");
+            }
+            else if (media >= 5.0 && media <= 6.9)
+            {
+                return ("c");
+            }
+            else if (media >= 7.0 && media <= 8.9)
+            {
+                return ("b");
+            }
+            else
+            {
+                return ("a");
+            }
+        }
+
         static void Main(string[] args)
         {
             double media;
             Console.WriteLine("Conceito média.");
             Console.WriteLine("digite a média final do aluno.");
             media = double.Parse(Console.ReadLine());
+            string retornaLetra = retornaConceito(media);
+            Console.WriteLine(retornaLetra);
+
         }
     }
 }
